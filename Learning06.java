008ca720cd3074ca3fbfcd76d796cace7cce6970
鈴木悠太 <suzukiyuuta@suzukiyuutanoMacBook-Air.local>
2024-05-23 14:21:43 +0900

    練習問題

---
+++ Learning06.java
@@ -0,0 +1,7 @@
+public class Learning06 {
+    public static void main(String[] args) {
+        int a = 3; int b = 5;
+        int c = a * b;
+        System.out.println("縦幅3横幅5の正方形の面積は," + c);
+    }
+}
