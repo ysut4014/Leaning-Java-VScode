dfd927ee402ec56502d089b790afea5b94ce120a
鈴木悠太 <suzukiyuuta@suzukiyuutanoMacBook-Air.local>
2024-06-02 23:34:50 +0900

    6/2配列

---
+++ Leaning25.java
@@ -0,0 +1,16 @@
+public class Leaning25 {
+    public static void main(String[] args) {
+        int sansu = 20;
+        int kokugo = 30;
+        int rika = 40;
+        int eigo = 50;
+        int syakai = 60;
+
+        int sum = sansu + kokugo + rika + eigo + syakai;
+
+        int avg =sum/5;
+        System.out.println("合計点:" + sum);
+        System.out.println("平均点" + avg);
+    }
+
+}
---
+++ Leaning26.java
@@ -0,0 +1,11 @@
+public class Leaning26 {
+    public static void main(String[] args) {
+        int[] scores;
+        scores = new int[5];
+        scores[1] = 30;
+        System.out.println(scores[1]);
+        System.out.println(scores[0]);
+        
+
+    }
+}
