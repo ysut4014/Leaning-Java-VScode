f4f6d723537ba54c882ab19cdce945895af66f93
鈴木悠太 <suzukiyuuta@suzukiyuutanoMacBook-Air.local>
2024-06-05 15:46:27 +0900

    6/5

---
+++ Leaning29.java
@@ -0,0 +1,13 @@
+public class Leaning29 {
+    public static void main(String[] args) {
+        int[] scores = {20, 30, 40, 50, 80};
+        int count = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] >= 50) {
+                count++;
+            }
+    }
+    System.out.println("50点以上の科目の数は:" + count);
+
+}
+}
